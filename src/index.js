907ec52fb1a52d6b80565f7bc4a319c755097a25
jlbeans <jm@cselmer.com>
2023-04-04 16:34:28 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { add } from "date-fns";
+import './style.css'
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = "Hello";
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
